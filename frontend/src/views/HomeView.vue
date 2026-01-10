e68cdf1c062df82050c4d4b8b5a0a6c584f52704
Alexander Roberts <a.roberts539@outlook.com>
2026-01-10 23:58:13 +0000

    Updated core aspects and appearance.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faFlask, faLightbulb, faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
+import { faFlask, faMicrochip, faPeopleGroup, faCode, faC } from '@fortawesome/free-solid-svg-icons';
 
 onMounted(async () => {
   const { gsap } = await import("gsap");
@@ -12,14 +12,14 @@ onMounted(async () => {
   <main class="dark:text-neutral-100 duration-500">
     <section class="w-full h-full dark:bg-[#2E4057] overflow-x-hidden">
       <div class="opening-content-div hidden lg:block">
-        <div class="absolute left-1/2 h-full bg-[#00C6B9] w-2 z-30"></div>
-        <div class="absolute top-1/2 w-full bg-[#00C6B9] h-2 z-30"></div>
-        <div class="absolute top-0 left-0 bottom-0 right-0 w-[26rem] h-48 m-auto rounded-full text-center bg-[#00C6B9] p-10 text-6xl z-40">
-          <span class="text-[#f8fafc]">ALEXANDER ROBERTS</span>
+        <div class="absolute left-1/2 h-full bg-[#ffeedd] dark:bg-[#3e5c76] w-2 z-30"></div>
+        <div class="absolute top-1/2 w-full bg-[#ffeedd] dark:bg-[#3e5c76] h-2 z-30"></div>
+        <div class="absolute top-0 left-0 bottom-0 right-0 w-[26rem] h-48 m-auto rounded-full text-center bg-[#ffeedd] dark:bg-[#3e5c76] py-[3.75rem] px-8 text-4xl z-40">
+          <span class="text-[#0e1720] dark:text-[#ffffff] font-bold">Hi 👋 I'm <span class="text-[#118ab2] dark:text-[#f5e8bb]">Alexander Roberts</span></span>
         </div>
       </div>
       <div class="w-screen h-full lg:h-screen z-10 flex justify-between flex-wrap flex-col lg:flex-row [&>div]:min-w-[50%] lg:[&>div]:w-[50%] [&>div]:w-full [&>div]:h-[50%] text-[#f8fafc]">
-        <div class="pt-48 pb-48 lg:pb-0 text-4xl md:text-5xl bg-[url('/frequency-wave.jpg')] text-center bg-cover">
+        <div class="py-60 lg:pb-0 text-4xl md:text-5xl bg-[url('/frequency-wave.jpg')] text-center bg-cover">
           <!-- 
           Image by Peace,love,happiness from Pixabay
           Available at: https://pixabay.com/illustrations/frequency-wave-geometry-pattern-7776034/
@@ -30,30 +30,32 @@ onMounted(async () => {
           <span class="font-bold"> Data Scientist </span>
           <FontAwesomeIcon :icon="faFlask" />
         </div>
-        <div class="pt-48 pb-48 lg:pb-0 text-4xl md:text-5xl text-center bg-[url('/idea-bulb.jpg')] bg-cover">
+        <div class="py-60 lg:pb-0 text-4xl md:text-5xl text-center bg-[url('/artificial-intelligence.jpg')] bg-cover">
           <!-- 
           Image by Pete Linforth from Pixabay
-          Available at: https://pixabay.com/illustrations/idea-light-bulb-innovation-4867554/
-          Creator: https://pixabay.com/users/thedigitalartist-202249/
-          Accessed: 6 January 2025
+          Available at: https://pixabay.com/illustrations/brain-artificial-intelligence-8530786/
+          Creator: https://pixabay.com/users/brianpenny-29844978
+          Accessed: 10 January 2026
           -->
-          <FontAwesomeIcon :icon="faLightbulb" />
-          <span class="font-bold"> Innovator </span>
-          <FontAwesomeIcon :icon="faLightbulb" />
+          <FontAwesomeIcon :icon="faMicrochip" />
+          <span class="font-bold"> AI Professional </span>
+          <FontAwesomeIcon :icon="faMicrochip" />
         </div>
-        <div class="pt-48 pb-48 lg:pb-0 text-4xl md:text-5xl text-center bg-[url('/puzzle.jpg')] bg-cover">
+        <div class="py-60 lg:pb-0 text-4xl md:text-5xl text-center bg-[url('/code-background.jpg')] bg-cover">
           <!-- 
           Image by PIRO from Pixabay
-          Available at: https://pixabay.com/illustrations/puzzle-connection-team-isolated-3d-7507357/
-          Creator: https://pixabay.com/users/piro4d-2707530/
-          Accessed: 6 January 2025
+          Available at: https://pixabay.com/illustrations/matrix-code-computer-pc-data-356024/
+          Accessed: 10 January 2026
           -->
+          <FontAwesomeIcon :icon="faCode" />
+          <span class="font-bold"> Software Engineer </span>
+          <FontAwesomeIcon :icon="faCode" />
+        </div>
+        <div class="py-60 lg:pb-0 text-4xl md:text-5xl text-center bg-[url('/puzzle.jpg')] bg-cover">
+          <!-- <img src="/selfie.jpg" alt="Selfie of author." class="w-[23rem] h-[28rem] m-auto my-4 rounded-full"> -->
           <FontAwesomeIcon :icon="faPeopleGroup" />
           <span class="font-bold"> Team Player </span>
           <FontAwesomeIcon :icon="faPeopleGroup" />
-        </div>
-        <div>
-          <img src="/selfie.jpg" alt="Selfie of author." class="w-[23rem] h-[28rem] m-auto my-4 rounded-full">
         </div>
       </div>
     </section>
