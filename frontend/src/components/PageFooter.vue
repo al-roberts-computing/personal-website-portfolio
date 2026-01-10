0c3a6fd3e3d23be6b67b50350d8bc6266e45b0dd
Alexander Roberts <a.roberts539@outlook.com>
2026-01-10 23:56:46 +0000

    Updated CV and email links.

--- frontend/src/components/PageFooter.vue
+++ frontend/src/components/PageFooter.vue
@@ -7,12 +7,12 @@ import { RouterLink } from 'vue-router';
 
 <template>
     <footer>
-        <div class="w-full flex justify-between items-center dark:bg-[#90E0EF] bg-[#0077B6] flex-col px-5 lg:flex-row md:px-12">
+        <div class="w-full flex justify-between items-center bg-[#0077B6] flex-col px-5 lg:flex-row md:px-12">
             <div class="py-4">
                 <span class="text-xl text-gray-100">Contact Me</span>
                 <ul class="[&>li]:py-1 [&>li>a:hover]:text-yellow-100 text-lg">
                     <li>
-                        Email: <a href="mailto:contact-me@alexander-roberts.me">contact-me@alexander-roberts.me</a>
+                        Email: <a href="mailto:a.roberts539@outlook.com">a.roberts539@outlook.com</a>
                     </li>
                     <li>
                         LinkedIn: <a href="https://www.linkedin.com/in/alexander-roberts-computing" target='__blank'>www.linkedin.com/in/alexander-roberts-computing</a>
@@ -26,7 +26,7 @@ import { RouterLink } from 'vue-router';
                 <RouterLink to="/terms-of-use" title="Want to know more?" aria-label="Have a look at my story.">
                     Terms of Use
                 </RouterLink>
-                <a href="/Alexander Roberts CV.pdf" download title="Download my CV." aria-label="My CV.">
+                <a href="/Alexander-Roberts-Public-CV.pdf" download title="Download my CV" aria-label="My CV">
                     Download CV <FontAwesomeIcon :icon="faDownload" />
                 </a>
                 <RouterLink to="/sitemap" title="Sitemap" aria-label="Check out this sitemap for an overview of this website.">
@@ -37,7 +37,7 @@ import { RouterLink } from 'vue-router';
         <div class="w-full flex justify-around flex-row items-center bg-blue-300 h-12">
             <span class="select-none">
                 <FontAwesomeIcon :icon="faCopyright" />
-                2024 Alexander Roberts.
+                2026 Alexander Roberts.
             </span>
 
             <a class="rounded-md bg-red-200 py-1 px-2 hover:text-green-200 hover:bg-purple-500" href="https://github.com/al-roberts-computing/personal-website-portfolio/tree/main" target="_blank">website source code</a>
