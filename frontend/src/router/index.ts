9a732da21c59b66d02f077aee880cbea5c30332a
Alexander Roberts <a.roberts539@outlook.com>
2026-01-21 11:00:35 +0000

    Router changes for handling invalid hyperlinks.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -48,6 +48,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/SitemapView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*', // This catches all routes that don't match above
+      name: 'not-found',
+      component: () => import('../views/PageNotFound.vue')
     }
   ]
 })
