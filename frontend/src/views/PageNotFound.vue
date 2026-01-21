8025b641f875c30394ea9948a79e32c655f99838
Alexander Roberts <a.roberts539@outlook.com>
2026-01-21 10:59:58 +0000

    Added a "page not found" view to manage invalid hyperlinks.

---
+++ frontend/src/views/PageNotFound.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="main-container w-full mt-40 mb-20 px-20 md:px-28 lg:px-4">
+        <div class="p-10 rounded-lg bg-slate-100 dark:bg-slate-800 shadow-md w-3/4 md:w-2/3 lg:w-1/3 text-center mx-auto">
+            <h1 class="text-5xl py-8 text-black dark:text-white">404 - Page Not Found</h1>
+            <p class="text-slate-600 mb-6 dark:text-slate-100">
+                Oops! The page you're looking for doesn't exist. Please check the URL or return to the <a href="/" class="text-blue-500 hover:underline">home page</a>.
+            </p>
+        </div>
+    </section>
+</template>
