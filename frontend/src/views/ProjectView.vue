3d6142f7b93b5184932b2f0a553c62475fd9c18b
Alexander Roberts <a.roberts539@outlook.com>
2026-02-01 22:08:23 +0000

    Updated styling and removed repo star count.

--- frontend/src/views/ProjectView.vue
+++ frontend/src/views/ProjectView.vue
@@ -38,7 +38,7 @@ onMounted(async () => {
 <template>
   <section class="feed-container w-full my-8 px-20 md:px-28 lg:px-44 lg:mt-20">
     <h1 class="text-5xl py-8 text-black dark:text-white">Projects</h1>
-    <p class="text-slate-600 mb-6 dark:text-slate-100">A selection of my recent projects hosted on GitHub. Click "Code" to view the source code or "Demo" to see it in action.</p>
+    <p class="text-slate-600 mb-6 dark:text-slate-100">A selection of my recent projects hosted on GitHub. Click "Code" to view the source code.</p>
     
     <div v-if="isLoading" class="loading text-xl p-6 bg-slate-400 text-blue-800 w-60 rounded-md text-center h-30 self-center mx-auto">
       <p>Fetching repos...</p>
@@ -54,22 +54,21 @@ onMounted(async () => {
           class="group p-6 bg-white border border-slate-200 rounded-2xl hover:border-blue-500 transition-all">
         
         <div class="flex justify-between items-start mb-4">
-          <h3 class="font-bold text-lg group-hover:text-blue-600 truncate">{{ repo.name }}</h3>
-          <span class="text-xs font-medium px-2 py-1 bg-slate-100 rounded-full">{{ repo.language }}</span>
+          <h3 class="font-bold text-lg group-hover:text-blue-600 truncate select-none">{{ repo.name }}</h3>
+          <span class="text-xs font-medium px-2 py-1 bg-slate-100 rounded-full select-none">{{ repo.language }}</span>
         </div>
 
-        <p class="text-slate-600 text-sm line-clamp-2 mb-4">
+        <p class="text-slate-600 text-sm line-clamp-2 mb-4 select-none">
           {{ repo.description }}
         </p>
 
         <div class="flex items-center gap-4 mt-auto">
           <a :href="repo.html_url" target="_blank" class="text-sm font-bold text-slate-900 hover:underline">Code</a>
-          <a v-if="repo.homepage" :href="repo.homepage" target="_blank" class="text-sm font-bold text-blue-600 hover:underline">Demo</a>
-          
+<!--           
           <div class="ml-auto flex items-center text-slate-400 text-xs">
             <svg class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" /></svg>
             {{ repo.stargazers_count }}
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
