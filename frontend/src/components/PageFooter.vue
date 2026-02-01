329760148635d449dfde97b00ffcaaef5c672780
Alexander Roberts <a.roberts539@outlook.com>
2026-02-01 22:06:02 +0000

    Updated footer styling.

--- frontend/src/components/PageFooter.vue
+++ frontend/src/components/PageFooter.vue
@@ -10,7 +10,7 @@ import { RouterLink } from 'vue-router';
         <div class="w-full flex justify-between items-center bg-[#0077B6] flex-col px-5 lg:flex-row md:px-12">
             <div class="py-4">
                 <span class="text-xl text-gray-100">Contact Me</span>
-                <ul class="[&>li]:py-1 [&>li>a:hover]:text-red-400 text-lg [&>li>a:visited]:text-pink-600 [&>li>a]:underline [&>li>a]:text-blue-100">
+                <ul class="[&>li]:py-1 [&>li>a:hover]:text-red-400 text-lg text-black font-bold [&>li>a:visited]:text-pink-600 [&>li>a]:underline [&>li>a]:text-blue-100">
                     <li>
                         Email: <a href="mailto:a.roberts539@outlook.com">a.roberts539@outlook.com</a>
                     </li>
@@ -37,7 +37,7 @@ import { RouterLink } from 'vue-router';
         <div class="w-full flex justify-around flex-row items-center bg-blue-300 h-12">
             <span class="select-none">
                 <FontAwesomeIcon :icon="faCopyright" />
-                2026 Alexander Roberts.
+                2026 Alexander Roberts
             </span>
 
             <a class="rounded-md bg-red-200 py-1 px-2 hover:text-green-200 hover:bg-purple-500" href="https://github.com/al-roberts-computing/personal-website-portfolio/tree/main" target="_blank">website source code</a>
